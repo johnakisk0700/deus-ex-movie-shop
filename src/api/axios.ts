39ad93f658ec0aea8bc15adfd4b2f3df421fd2f8
johnakisk0700 <johnnstavv@gmail.com>
2022-11-25 14:06:41 +0200

    [fix]: added VITE_ prefix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.PROXY_URL,
+  baseURL: import.meta.env.VITE_PROXY_URL,
 });
 
 export const privateAxiosInstance = axios.create({
-  baseURL: import.meta.env.PROXY_URL,
+  baseURL: import.meta.env.VITE_PROXY_URL,
   withCredentials: true,
 });
