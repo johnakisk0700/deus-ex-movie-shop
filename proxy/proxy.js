d885e8c7bd771e047c1482951cddc64dd39c5589
johnakisk0700 <johnnstavv@gmail.com>
2022-11-25 13:12:52 +0200

    [feature]: added .env to proxy

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,10 +1,11 @@
 var proxy = require("express-http-proxy");
 var app = require("express")();
+require("dotenv").config();
 
 var cors = require("cors");
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-app.use("/", proxy("http://3.235.214.44:8000/"));
+app.use("/", proxy(process.env.BACKEND_URL));
 
 const port = 5050;
 app.listen(port, () => {
